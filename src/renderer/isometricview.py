32aeedb966521c521d90231a3e18abe7f20bd330
Tengda <yantd@qq.com>
2024-03-09 21:33:43 +0800

    Depth

--- src/renderer/isometricview.py
+++ src/renderer/isometricview.py
@@ -3,8 +3,20 @@ import math
 import numpy as np
 from datasource import telemetry
 
+class Polygon:
+    Depth: float
+    Points: np.matrix
+    def __init__(self, Points:np.matrix):
+        self.Points = Points
+        self.Depth = sum(Points[:,1])[0,0] / len(Points)
+
+    def RefreshDepth(self):
+        self.Depth = sum(self.Points[:,1])[0,0] / len(self.Points)
+
+
 class CourseData:
     Course: np.matrix
+    Polygons: list[Polygon]
     Shadow: np.matrix
     Rotation: np.matrix
     MaxX: float
@@ -28,10 +40,11 @@ class CourseData:
             ptlist.append(pti)
             self.Rows += 1
         self.Course = np.matrix(ptlist)
+        self.CourseToPolygons()
         self.Shadow = self.Course.copy()
         self.Shadow[:,2] = 0
 
-    def SetRotationMatrix(self, alpha, beta):
+    def SetRotationMatrix(self, alpha, beta, imageScale):
         """Rotate along Z axis for alpha rad. Then along X azis for beta rad."""
         sa = math.sin(alpha)
         ca = math.cos(alpha)
@@ -44,28 +57,35 @@ class CourseData:
         rot2 = np.matrix([[ 1,   0,  0],
                           [ 0,  cb, sb],
                           [ 0, -sb, cb]])
-        self.Rotation = np.dot(rot1, rot2)
+        
+        scale3 = np.matrix([[ imageScale, 0, 0],
+                            [ 0, imageScale, 0],
+                            [ 0, 0, 0.0-imageScale]])
+        
+        self.Rotation = np.dot(np.dot(rot1, rot2), scale3)
         return self.Rotation
     
     def Projection(self, point):
         p = np.dot(point, self.Rotation)
-        return (p[0,0] * 200 + 200, p[0,2] * (-200) + 350)
+        return (p[0,0] + 200, p[0,2] + 350)
+
+    def CourseToPolygons(self):
+        self.Polygons = []
+        ptNumbers = len(self.Course)
+        for i in range(1, ptNumbers):
+            polygon = []
+            polygon.append([self.Course[i-1,0], self.Course[i-1,1], 0])
+            polygon.append([self.Course[i-1,0], self.Course[i-1,1], self.Course[i-1,2]])
+            polygon.append([self.Course[i,  0], self.Course[i,  1], self.Course[i,2]])
+            polygon.append([self.Course[i,  0], self.Course[i,  1], 0])
+            self.Polygons.append(Polygon(np.matrix(polygon)))
 
     def Draw(self, draw:ImageDraw.ImageDraw):
-        lcourse = np.dot(self.Course, self.Rotation)
-        lcourseProj = lcourse[:,[0,2]] * [[200,0],[0, -200]] + [200, 350]
-        lshadow = np.dot(self.Shadow, self.Rotation)
-        lshadowProj = lshadow[:,[0,2]]  * [[200,0],[0, -200]] + [200, 350]
-        dt1 = []
-        dt2 = []
-        for i in range(0, self.Rows):
-            pt1 = (lcourseProj[i, 0], lcourseProj[i, 1])
-            pt2 = (lshadowProj[i, 0], lshadowProj[i, 1])
-            dt1.append(pt1)
-            dt2.append(pt2)
-            draw.line([pt1, pt2], fill="#CCCCCCFF", width=1)
-        draw.line(dt1, fill="#CCCCCCFF", width=3)
-        draw.line(dt2, fill="#CCCCCCFF", width=3)
+        rotated = []
+        for p in self.Polygons:
+            p1 = Polygon(np.dot(p.Points, self.Rotation))
+            rotated.append(p1)
+
         box = [
             self.Projection([-self.MaxX, self.MaxY, 0]),
             self.Projection([self.MaxX, self.MaxY, 0]),
@@ -74,6 +94,20 @@ class CourseData:
             self.Projection([-self.MaxX, self.MaxY, 0])
         ]
         draw.line(box, fill="#CCCCCCFF", width=1)
+        rotated.sort(key=lambda k: - k.Depth)
+        for p in rotated:
+            draw.polygon(
+                [
+                    (p.Points[0,0] + 200, p.Points[0,2] + 350),
+                    (p.Points[1,0] + 200, p.Points[1,2] + 350),
+                    (p.Points[2,0] + 200, p.Points[2,2] + 350),
+                    (p.Points[3,0] + 200, p.Points[3,2] + 350),
+                ],
+                fill   ='#cccccc00',
+                outline='#ffffffff',
+                width=1
+            )
+
 
 
 class IsometricImg:
@@ -91,24 +125,15 @@ class IsometricImg:
 
         self.BackGround = Image.new('RGBA', (400, 400), '#00000000')
         draw = ImageDraw.Draw(self.BackGround)
-        
         self.Font = ImageFont.truetype('/Users/tengdayan/Code/Gpx2Img/Tachyo-1.0.0.otf', size=50)
         textFont = ImageFont.truetype('Arial', size=30)
         draw.text([80,90 + self.TextOffset], 'km/h', fill='#EEEEEEFF', font=textFont)
         draw.text([295,90 + self.TextOffset], 'm', fill='#EEEEEEFF', font=textFont)
 
-    def RenderPreview(self, filename):
-        img = self.BackGround.copy()
-        draw = ImageDraw.Draw(img)
-        self.Course.SetRotationMatrix(0.0 - np.pi / 6, np.pi / 8)
-        self.Course.Draw(draw)
-        self.BackGround.save(filename)
-
-
     def RenderFrame(self, filename, time:float):
         img = self.BackGround.copy()
         draw = ImageDraw.Draw(img)
-        self.Course.SetRotationMatrix( -0.05 * time - np.pi / 6, np.pi / 8)
+        self.Course.SetRotationMatrix( -0.05 * time - np.pi / 6, np.pi / 8, 200)
         self.Course.Draw(draw)
         for i in range(0, len(self.data.DataPoints) - 1, 1):
             if self.data.DataPoints[i].TimeOffset <= time and self.data.DataPoints[i+1].TimeOffset > time :
